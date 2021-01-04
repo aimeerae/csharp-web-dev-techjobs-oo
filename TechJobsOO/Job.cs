b07ca4c5d5e54108993d1c509836411894ca7132
Aimee Bhasme <aimee.rae612@gmail.com>
2021-01-03 18:57:47 -0600

    job class

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -4,7 +4,9 @@ namespace TechJobsOO
     public class Job
     {
         public int Id { get; }
-        private static int nextId = 1;
+        
+        private static int nextId;
+       
 
         public string Name { get; set; }
         public Employer EmployerName { get; set; }
@@ -13,7 +15,39 @@ namespace TechJobsOO
         public CoreCompetency JobCoreCompetency { get; set; }
 
         // TODO: Add the two necessary constructors.
+        
+        public Job()
+        {
+            Id = nextId;
+            nextId += 1;
+        }
 
+        public Job(string name, Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCoreCompetency)
+        { 
+            Name = name
+            EmployerName = employerName
+            EmployerLocation = employerLocation
+            JobType = jobType
+            JobCoreCompetency = jobCoreCompetency
+        }
         // TODO: Generate Equals() and GetHashCode() methods.
+         public override bool Equals(object obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+        
+        
+        
+       
+
+
+
     }
 }
